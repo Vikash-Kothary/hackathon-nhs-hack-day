fca2e92e8e73ceae2174bd81c97b1d8fc6e77f03
Vikash Kothary <kothary.vikash@gmail.com>
2024-10-13 12:43:20 +0100

    feat: Add more task types

--- src/backend/nhs_hack_day/models.py
+++ src/backend/nhs_hack_day/models.py
@@ -1,7 +1,11 @@
 from enum import Enum
 
-class Speciality(Enum):
-    UNKNOWN = "Unknown"
+class TaskType(Enum):
     RESPIRATORY_MEDICINE = "Respiratory medicine"
     GASTROENTEROLOGY = "Gastroenterology"
     MICROBIOLOGY = "Microbiology"
+    ADD_ON_BLOODS_TEST = "Add-on bloods test"
+    REFER_INPATIENT_MEDICAL_SPECIALITY = "Refer to inpatient medical speciality"
+    REFER_OUTPATIENT_CLINIC = "Refer to outpatient clinic"
+    DECIDE_ON_PRESCRIPTION = "Decide on prescription"
+    UNKNOWN = "Unknown"
--- src/backend/nhs_hack_day/services/task_service.py
+++ src/backend/nhs_hack_day/services/task_service.py
@@ -1,25 +1,37 @@
 import uuid
 
-from nhs_hack_day.models import Speciality
+from nhs_hack_day.models import TaskType
 from nhs_hack_day.repositories import task_repository
 
-def get_speciality(speciality_string: str) -> Speciality:
+def get_task_type(speciality_string: str) -> TaskType:
     speciality_string = speciality_string.lower()
     
     if "resp" in speciality_string or "respiratory" in speciality_string:
-        return Speciality.RESPIRATORY_MEDICINE
+        return TaskType.RESPIRATORY_MEDICINE
     elif "gastro" in speciality_string or "gastroenterology" in speciality_string:
-        return Speciality.GASTROENTEROLOGY
+        return TaskType.GASTROENTEROLOGY
     elif "micro" in speciality_string or "microbiology" in speciality_string:
-        return Speciality.MICROBIOLOGY
-    return Speciality.UNKNOWN
+        return TaskType.MICROBIOLOGY
+    elif "add" in speciality_string or "blood" in speciality_string:
+        return TaskType.ADD_ON_BLOODS_TEST
+    elif "refer" in speciality_string and ("inpatient" in speciality_string or "d/c" in speciality_string or "r/f" in speciality_string):
+        return TaskType.REFER_INPATIENT_MEDICAL_SPECIALITY
+    elif "outpatient" in speciality_string or "clinic" in speciality_string or "appointment" in speciality_string:
+        return TaskType.REFER_OUTPATIENT_CLINIC
+    elif ("prescribe" in speciality_string or "drug" in speciality_string or 
+          "iv" in speciality_string or "antibiotics" in speciality_string or 
+          "abx" in speciality_string or "analgesia" in speciality_string or 
+          "anti-emetic" in speciality_string or "laxative" in speciality_string):
+        return TaskType.DECIDE_ON_PRESCRIPTION
+    
+    return TaskType.UNKNOWN
 
 
 def create_new_task(task_description: str):
     task = {}
     task['task_id'] = str(uuid.uuid4())
     task['description'] = task_description
-    task['speciality'] = get_speciality(task_description)
+    task['task_type'] = get_task_type(task_description)
     # task['name'] = get_task_name()
     task_repository.create_new_task(task)
     return task
